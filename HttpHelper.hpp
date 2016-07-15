1ba9705e8859b94e52a3668c1107d2f192fc9219
ivansanzcarasa <none@none>
2016-07-15 11:31:47 +0200

    [main] Create main
    --HG--
    branch : main

---
+++ HttpHelper.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+
+class HttpHelper
+{
+public:
+	static std::string getHeaderValue(std::string allHeaders, std::string headerName)
+	{
+		std::string line;
+		do
+		{
+			line = allHeaders.substr(0, allHeaders.find_first_of("\r\n"));
+			if (line.find(headerName + ":") != std::string::npos)
+			{
+				std::string value = line.substr(headerName.size() + 1);
+				while(value[0] == ' ' || value[0] == '\t' || value[0] == '\r' || value[0] == '\n')
+				{
+					value = value.substr(1);
+				}
+				return value;
+			}
+
+			int newPos = allHeaders.find_first_of("\r\n");
+			if (newPos != std::string::npos)
+			allHeaders = allHeaders.substr(newPos + 2);
+		} 
+		while (allHeaders.find("\r\n") != std::string::npos);
+
+		return "";
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,79 @@
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "wsock32.lib")
+
+#include <iostream>
+#include <thread>
+#include "Sockets.hpp"
+#include "HttpHelper.hpp"
+#include "Sha1.hpp"
+#include "Base64Helper.hpp"
+#include <sstream>
+
+//void client();
+void server();
+
+int main(int argc, char** argv)
+{
+	std::thread tServer(server);
+	tServer.join();
+	
+	std::cout << "Finished" << std::endl;
+	std::cin.get();
+
+	return 0;
+}
+
+/*void client()
+{
+	TCPClient* client = new TCPClient();
+	client->connect("127.0.0.1", 66453);
+	client->setBlocking(true);
+
+	client->send("Hola");
+
+	while (true)
+	{
+		string buffer = client->recv();
+		std::cout << "Client >> " << buffer << std::endl;
+	}
+
+	delete client;
+}*/
+
+void server()
+{
+	TCPRawServer* server = new TCPRawServer();
+	if (!server->start(80))
+	{
+		std::cout << "Error" << std::endl;
+		return;
+	}
+	std::cout << "Server started @ 80" << std::endl;
+	server->setBlocking(true);
+
+	while (true)
+	{
+		Connection client1 = server->newClient();
+		TCPClient* client = new TCPClient();
+		client->connect(client1.sock, client1.ip, 80);
+		std::cout << "Client connected!" << std::endl;
+
+		bool a = false;
+		while (client->isConnected()) 
+		{
+			std::string buffer = client->recv();
+			if (!a) {
+				a = true;
+				std::string websocketKey = HttpHelper::getHeaderValue(buffer, "Sec-WebSocket-Key");
+				std::cout << "Handshake Key: " << websocketKey << std::endl;
+				std::string sha1Key = sha1UnsignedChar(websocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+				std::string finalKey = Base64Helper::encode(sha1Key.c_str());
+
+				client->send("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Protocol: chat\r\nSec-WebSocket-Accept: " + finalKey + "\r\n\r\n");
+				std::cout << "HandShake done" << std::endl;
+			}
+		}
+	}
+
+	delete server;
+}
